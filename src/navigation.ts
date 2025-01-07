a7416472e7adaed0727088c8fdbfcadeff0d0751
YASWANTH KANAKALA <82636668+yeswanthkanakala07@users.noreply.github.com>
2025-01-07 12:40:21 -0800

    Updated Index.js
    Relevant info placed.

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,7 +3,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Services',
       links: [
         {
           text: 'SaaS',
@@ -12,14 +12,6 @@ export const headerData = {
         {
           text: 'Startup',
           href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
         },
       ],
     },
@@ -177,7 +169,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    
+    Made by © 2025 Digital Minds Consulting. All rights reserved. 
   `,
 };
