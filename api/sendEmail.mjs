47dee01c172059913c64d4f036ce1adbe1b9786a
YASWANTH KANAKALA <82636668+yeswanthkanakala07@users.noreply.github.com>
2025-01-07 18:44:01 -0800

    Update sendEmail.mjs

--- api/sendEmail.mjs
+++ api/sendEmail.mjs
@@ -1,24 +1,43 @@
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground' // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
 
 export default async (req, res) => {
   const { name, email, message } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: email,
-    to: 'yaswanth07kanakala@gmail.com',
-    subject: `Contact form submission from ${name}`,
-    text: message,
-  };
-
   try {
+    const accessToken = await oauth2Client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken.token,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: 'yeswanth2001kanakala@gmail.com',
+      subject: `Contact form submission from ${name}`,
+      text: message,
+    };
+
     await transporter.sendMail(mailOptions);
     res.status(200).send('Message sent successfully!');
   } catch (error) {
